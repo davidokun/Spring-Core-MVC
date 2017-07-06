0f5793d6cc0a70f911360beeaafde66494109fbf
davidokun <davidokun99@gmail.com>
2017-07-06 15:52:12 -0500

    Add @ComponentScan annotation to main Java Config class

--- simple-spring-java-config/src/main/java/com/singletonapps/config/JavaConfig.java
+++ simple-spring-java-config/src/main/java/com/singletonapps/config/JavaConfig.java
@@ -3,7 +3,9 @@ package com.singletonapps.config;
 import com.singletonapps.service.BlogPostService;
 import com.singletonapps.service.impl.BlogPostServiceImpl;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {"com.singletonapps"})
 public class JavaConfig {
 
     @Bean /* Default Bean ID is method name */
