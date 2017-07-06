3baefa1b3c647c2bcc7adc96f28221af859817a5
davidokun <davidokun99@gmail.com>
2017-07-06 15:48:04 -0500

    Adding new model class for DataSource

---
+++ simple-spring-java-config/src/main/java/com/singletonapps/domain/DataSource.java
@@ -0,0 +1,32 @@
+package com.singletonapps.domain;
+
+public class DataSource {
+
+    private String url;
+    private String userName;
+    private String passWord;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+}
