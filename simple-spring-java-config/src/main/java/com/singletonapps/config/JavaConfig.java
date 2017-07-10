6d638658d0bac5929be0bd3af547f9b24a515e1c
davidokun <davidokun99@gmail.com>
2017-07-10 17:25:46 -0500

    Adding @Import of class EmailServiceConfig into main configuration class (JavaConfig)

--- simple-spring-java-config/src/main/java/com/singletonapps/config/JavaConfig.java
+++ simple-spring-java-config/src/main/java/com/singletonapps/config/JavaConfig.java
@@ -1,11 +1,14 @@
 package com.singletonapps.config;
 
+import com.singletonapps.other.EmailServiceConfig;
 import com.singletonapps.service.BlogPostService;
 import com.singletonapps.service.impl.BlogPostServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
-@ComponentScan(basePackages = {"com.singletonapps"})
+@ComponentScan(basePackages = {"com.singletonapps.config"})
+@Import(value = {EmailServiceConfig.class})
 public class JavaConfig {
 
     @Bean /* Default Bean ID is method name */
