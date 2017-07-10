2e855c6aa6b48c511a92093dde2d5f9eba0e331e
davidokun <davidokun99@gmail.com>
2017-07-10 17:32:57 -0500

    Using @Autowired to Inject bean DataSource into BlogPostServiceImpl.

--- simple-spring-java-config/src/main/java/com/singletonapps/service/impl/BlogPostServiceImpl.java
+++ simple-spring-java-config/src/main/java/com/singletonapps/service/impl/BlogPostServiceImpl.java
@@ -1,17 +1,23 @@
 package com.singletonapps.service.impl;
 
 import com.singletonapps.domain.BlogPost;
+import com.singletonapps.domain.DataSource;
 import com.singletonapps.service.BlogPostService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class BlogPostServiceImpl implements BlogPostService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(BlogPostServiceImpl.class);
 
+    @Autowired
+    private DataSource dataSource;
+
     @Override
     public void savePost(BlogPost blogPost) {
 
         LOGGER.debug("BlogPostServiceImpl: savePost is called");
+        LOGGER.debug("BlogPostServiceImpl: " + dataSource.getUrl());
     }
 }
