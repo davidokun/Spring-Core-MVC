644103d6ec7f5d8a87a687bf8a7fc1293a476d82
davidokun <davidokun99@gmail.com>
2017-07-14 21:42:26 -0500

    Adding multiple @Bean definitions for BlogPostService implementation.

--- simple-spring-java-config/src/main/java/com/singletonapps/config/JavaConfig.java
+++ simple-spring-java-config/src/main/java/com/singletonapps/config/JavaConfig.java
@@ -1,18 +1,32 @@
 package com.singletonapps.config;
 
-import com.singletonapps.other.EmailServiceConfig;
+import com.singletonapps.other.ServiceConfig;
 import com.singletonapps.service.BlogPostService;
 import com.singletonapps.service.impl.BlogPostServiceImpl;
+import com.singletonapps.service.impl.MediumBlogServiceImpl;
+import com.singletonapps.service.impl.WordPressBlogServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
 @ComponentScan(basePackages = {"com.singletonapps.config"})
-@Import(value = {EmailServiceConfig.class})
+@Import(value = {ServiceConfig.class})
 public class JavaConfig {
 
     @Bean /* Default Bean ID is method name */
     public BlogPostService blogPostService() {
         return new BlogPostServiceImpl();
+    }
+
+    @Bean(name = "WordPressBlogPostService")
+    public BlogPostService wordPressBlogPostService() {
+
+        return new WordPressBlogServiceImpl();
+    }
+
+    @Bean(name = "MediumBlogPostService")
+    public BlogPostService mediumBlogPostService() {
+
+        return new MediumBlogServiceImpl();
     }
 }
