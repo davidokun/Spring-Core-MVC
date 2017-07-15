59efddfcb430e728febd5224a89706961d053bf4
davidokun <davidokun99@gmail.com>
2017-07-14 21:36:12 -0500

    Adding a ServiceManager @Bean method into configuration.

--- simple-spring-java-config/src/main/java/com/singletonapps/other/ServiceConfig.java
+++ simple-spring-java-config/src/main/java/com/singletonapps/other/ServiceConfig.java
@@ -1,16 +1,24 @@
 package com.singletonapps.other;
 
 import com.singletonapps.service.EmailService;
+import com.singletonapps.service.ServiceManager;
 import com.singletonapps.service.impl.EmailServiceImpl;
+import com.singletonapps.service.impl.ServiceManagerImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class EmailServiceConfig {
+public class ServiceConfig {
 
     @Bean
     public EmailService emailService() {
 
         return new EmailServiceImpl();
+    }
+
+    @Bean
+    public ServiceManager serviceManager() {
+
+        return new ServiceManagerImpl();
     }
 }
