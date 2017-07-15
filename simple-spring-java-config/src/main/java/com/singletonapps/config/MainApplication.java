0d1d8c95175ea6dbcf10a7226221748b1ce04749
davidokun <davidokun99@gmail.com>
2017-07-14 21:48:20 -0500

    Adding ServiceManager test.

--- simple-spring-java-config/src/main/java/com/singletonapps/config/MainApplication.java
+++ simple-spring-java-config/src/main/java/com/singletonapps/config/MainApplication.java
@@ -4,6 +4,7 @@ import com.singletonapps.domain.BlogPost;
 import com.singletonapps.domain.DataSource;
 import com.singletonapps.service.BlogPostService;
 import com.singletonapps.service.EmailService;
+import com.singletonapps.service.ServiceManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
@@ -20,10 +21,10 @@ public class MainApplication {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(JavaConfig.class);
 
         /* Getting Bean by type */
-        BlogPostService blogPostService = applicationContext.getBean(BlogPostService.class);
+//        BlogPostService blogPostService = applicationContext.getBean(BlogPostService.class);
 
         /* Getting bean by method name (Default Bean Id) */
-//        BlogPostService blogPostService = (BlogPostService) applicationContext.getBean("blogPostService");
+        BlogPostService blogPostService = (BlogPostService) applicationContext.getBean("blogPostService");
 
         BlogPost blogPost = new BlogPost();
         blogPost.setId(1L);
@@ -40,6 +41,10 @@ public class MainApplication {
         EmailService emailService = applicationContext.getBean(EmailService.class);
 
         emailService.sendEmail();
+
+        ServiceManager serviceManager = applicationContext.getBean(ServiceManager.class);
+
+        serviceManager.sendBlogPost(blogPost);
 
 
         ((ConfigurableApplicationContext) applicationContext).close();
