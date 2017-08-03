bfc4e3ab5e94f5aca12d3a7901029a3b489ce645
davidokun <davidokun99@gmail.com>
2017-08-02 19:50:12 -0500

    Configuring the resource handler for static files like .css and .js

--- spring-mvc-java-config/src/main/java/com/singletonapps/config/WebConfig.java
+++ spring-mvc-java-config/src/main/java/com/singletonapps/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.singletonapps.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -18,6 +19,12 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     public void addViewControllers(ViewControllerRegistry registry) {
 
         registry.addViewController("/").setViewName("index");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
     }
 
     @Bean
