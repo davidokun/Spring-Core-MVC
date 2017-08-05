cec95668cba5be49be4132b44c0d81cbba57b521
davidokun <davidokun99@gmail.com>
2017-08-04 19:34:12 -0500

    Added new domain class for authority.

---
+++ spring-mvc-java-config/src/main/java/com/singletonapps/domain/Authority.java
@@ -0,0 +1,32 @@
+package com.singletonapps.domain;
+
+public class Authority {
+
+    private Long id;
+    private String userName;
+    private String authority;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+}
