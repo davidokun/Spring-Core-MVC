562d5a00884bc83c55657a6a77dd0792db68945d
davidokun <davidokun99@gmail.com>
2017-08-13 14:32:34 -0500

    Adding @PropertySource location for properties file. Adding @Bean for PropertySourcesPlaceholderConfigurer.

--- spring-mvc-java-config/src/main/java/com/singletonapps/config/WebConfig.java
+++ spring-mvc-java-config/src/main/java/com/singletonapps/config/WebConfig.java
@@ -1,9 +1,7 @@
 package com.singletonapps.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.*;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -17,6 +15,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @Configuration
 @Import({ApplicationContext.class})
 @ComponentScan(basePackages = "com.singletonapps")
+@PropertySource(value = "classpath:application.properties")
 public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
@@ -29,6 +28,12 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
         registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+
+        return new PropertySourcesPlaceholderConfigurer();
     }
 
     @Bean
