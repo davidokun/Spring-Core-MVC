4f92e309e072b9c67d6166592caf0e18ed9da472
davidokun <davidokun99@gmail.com>
2017-08-14 20:18:24 -0500

    Adding new Controller for MySql database

---
+++ spring-mvc-java-config/src/main/java/com/singletonapps/controller/DatabaseController.java
@@ -0,0 +1,36 @@
+package com.singletonapps.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.sql.DataSource;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class DatabaseController {
+
+    private static Logger LOGGER = LoggerFactory.getLogger(DatabaseController.class);
+
+    @Autowired
+    private DataSource dataSource;
+
+    private JdbcTemplate jdbcTemplate;
+
+    @RequestMapping(value = "/displayUsersMySQL", method = RequestMethod.GET)
+    public void displayUsers() {
+
+        /* MySQL DATABASE */
+        jdbcTemplate = new JdbcTemplate(dataSource);
+
+        List<Map<String, Object>> users = jdbcTemplate.queryForList("SELECT * FROM USER");
+
+        users
+                .forEach(user -> LOGGER.debug("username: " + user.get("userName")));
+    }
+}
