b1f1345e2017e7bb0709099779e2276e5ea3b5ef
davidokun <davidokun99@gmail.com>
2017-08-23 19:05:52 -0500

    Adding Hibernate Java Configuration in DataBaseContext class.

--- spring-mvc-java-config/src/main/java/com/singletonapps/config/DataBaseContext.java
+++ spring-mvc-java-config/src/main/java/com/singletonapps/config/DataBaseContext.java
@@ -8,8 +8,15 @@ import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 public class DataBaseContext {
@@ -42,5 +49,45 @@ public class DataBaseContext {
                 .addScript("dbschema.sql")
                 .addScript("test-data.sql")
                 .build();
+    }
+
+    @Bean
+    public JpaTransactionManager jpaTransactionManager(EntityManagerFactory entityManagerFactory) {
+
+        JpaTransactionManager jpaTransactionManager = new JpaTransactionManager();
+
+        jpaTransactionManager.setEntityManagerFactory(entityManagerFactory);
+
+        return jpaTransactionManager;
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+
+        HibernateJpaVendorAdapter hibernateJpaVendorAdapter =
+                new HibernateJpaVendorAdapter();
+
+        hibernateJpaVendorAdapter.setDatabase(Database.MYSQL);
+        hibernateJpaVendorAdapter.setShowSql(Boolean.TRUE);
+
+        return hibernateJpaVendorAdapter;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactoryBean() {
+
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean =
+                new LocalContainerEntityManagerFactoryBean();
+
+        entityManagerFactoryBean.setDataSource(dataSource());
+        entityManagerFactoryBean.setJpaVendorAdapter(jpaVendorAdapter());
+        entityManagerFactoryBean.setPackagesToScan("com.sinlgetonapps.domain");
+
+        Properties jpaProperties = new Properties();
+        jpaProperties.setProperty("hibernate.hbm2ddl.auto", "create-drop");
+
+        entityManagerFactoryBean.setJpaProperties(jpaProperties);
+
+        return entityManagerFactoryBean;
     }
 }
