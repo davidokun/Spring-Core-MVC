0e69c0967d4ca9b02359005d1af748aa02c173d3
davidokun <davidokun99@gmail.com>
2017-08-25 19:20:52 -0500

    Adding crud operation to BlogPostService interface

--- spring-mvc-java-config/src/main/java/com/singletonapps/service/BlogPostService.java
+++ spring-mvc-java-config/src/main/java/com/singletonapps/service/BlogPostService.java
@@ -1,8 +1,21 @@
 package com.singletonapps.service;
 
 import com.singletonapps.domain.BlogPost;
+import com.singletonapps.domain.User;
+
+import java.util.List;
 
 public interface BlogPostService {
 
     void savePost(BlogPost blogPost);
+
+    void saveAsDraft(BlogPost blogPost);
+
+    void deletePost(BlogPost blogPost);
+
+    List<BlogPost> listAllBlogPostsByUserAndDraftStatus(User user, boolean draft);
+
+    List<BlogPost> listAllBlogPostsByUserAndTitleLike(User user, String title);
+
+    BlogPost findBlogPostById(Long id);
 }
