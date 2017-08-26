1ab0e3c333e6599b3c7f3c005d4c6560ed856a3e
davidokun <davidokun99@gmail.com>
2017-08-25 19:25:52 -0500

    Implemeting methods from BlogPostService in BlogPostServiceImpl

--- spring-mvc-java-config/src/main/java/com/singletonapps/service/impl/BlogPostServiceImpl.java
+++ spring-mvc-java-config/src/main/java/com/singletonapps/service/impl/BlogPostServiceImpl.java
@@ -1,23 +1,58 @@
 package com.singletonapps.service.impl;
 
+import com.singletonapps.dao.BlogPostDAO;
 import com.singletonapps.domain.BlogPost;
-import com.singletonapps.domain.DataSource;
+import com.singletonapps.domain.User;
 import com.singletonapps.service.BlogPostService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Date;
+import java.util.List;
 
 public class BlogPostServiceImpl implements BlogPostService {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(BlogPostServiceImpl.class);
 
     @Autowired
-    private DataSource dataSource;
+    private BlogPostDAO blogPostDAO;
 
     @Override
     public void savePost(BlogPost blogPost) {
 
         LOGGER.debug("BlogPostServiceImpl: savePost is called");
-        LOGGER.debug("BlogPostServiceImpl: " + dataSource.getUrl());
+
+        blogPost.setPublishDate(new Date());
+        blogPostDAO.save(blogPost);
+    }
+
+    @Override
+    public void saveAsDraft(BlogPost blogPost) {
+        blogPost.setDraft(Boolean.TRUE);
+        blogPostDAO.save(blogPost);
+    }
+
+    @Override
+    public void deletePost(BlogPost blogPost) {
+        blogPostDAO.delete(blogPost);
+    }
+
+    @Override
+    public List<BlogPost> listAllBlogPostsByUserAndDraftStatus(User user, boolean draft) {
+
+        return null;
+    }
+
+    @Override
+    public List<BlogPost> listAllBlogPostsByUserAndTitleLike(User user, String title) {
+
+        return null;
+    }
+
+    @Override
+    public BlogPost findBlogPostById(Long id) {
+
+        return blogPostDAO.findOne(id);
     }
 }
