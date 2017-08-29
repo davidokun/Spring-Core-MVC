acc8f40ce97c30caab2014a52848ea19a37f1e97
davidokun <davidokun99@gmail.com>
2017-08-29 18:35:52 -0500

    Implementing methods in WordPressBlogServiceImpl.java

--- spring-mvc-java-config/src/main/java/com/singletonapps/service/impl/WordPressBlogServiceImpl.java
+++ spring-mvc-java-config/src/main/java/com/singletonapps/service/impl/WordPressBlogServiceImpl.java
@@ -1,9 +1,12 @@
 package com.singletonapps.service.impl;
 
 import com.singletonapps.domain.BlogPost;
+import com.singletonapps.domain.User;
 import com.singletonapps.service.BlogPostService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 public class WordPressBlogServiceImpl implements BlogPostService {
 
@@ -13,5 +16,30 @@ public class WordPressBlogServiceImpl implements BlogPostService {
     public void savePost(BlogPost blogPost) {
 
         LOGGER.debug("WordPressBlogServiceImpl : savePost is called!");
+    }
+
+    @Override
+    public void saveAsDraft(BlogPost blogPost) {
+
+    }
+
+    @Override
+    public void deletePost(BlogPost blogPost) {
+
+    }
+
+    @Override
+    public List<BlogPost> listAllBlogPostsByUserAndDraftStatus(User user, boolean draft) {
+        return null;
+    }
+
+    @Override
+    public List<BlogPost> listAllBlogPostsByUserAndTitleLike(User user, String title) {
+        return null;
+    }
+
+    @Override
+    public BlogPost findBlogPostById(Long id) {
+        return null;
     }
 }
