ef40f3d777873d181327adde221af3971b185030
davidokun <davidokun99@gmail.com>
2017-09-05 15:56:41 -0500

    Adding UserService implementation.

---
+++ spring-mvc-java-config/src/main/java/com/singletonapps/service/impl/UserServiceImpl.java
@@ -0,0 +1,24 @@
+package com.singletonapps.service.impl;
+
+import com.singletonapps.dao.UserDAO;
+import com.singletonapps.domain.User;
+import com.singletonapps.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserDAO userDAO;
+
+    @Override
+    public User findUserById(long id) {
+        return userDAO.findOne(id);
+    }
+
+    @Override
+    public User findUserWithBlogPostsByUsername(String userName) {
+        return null;
+    }
+}
