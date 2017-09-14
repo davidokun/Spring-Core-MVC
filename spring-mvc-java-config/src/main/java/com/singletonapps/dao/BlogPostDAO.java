a959893ea6a91f9d9aa2f947b94bf53221b728ad
davidokun <davidokun99@gmail.com>
2017-09-13 19:32:41 -0500

    Adding custom queries to BlogPostDAO.java

--- spring-mvc-java-config/src/main/java/com/singletonapps/dao/BlogPostDAO.java
+++ spring-mvc-java-config/src/main/java/com/singletonapps/dao/BlogPostDAO.java
@@ -1,8 +1,14 @@
 package com.singletonapps.dao;
 
 import com.singletonapps.domain.BlogPost;
+import com.singletonapps.domain.User;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
 
 public interface BlogPostDAO extends CrudRepository<BlogPost, Long> {
 
+    List<BlogPost> findAllBlogPostByUserAndTitleContaining(User user, String title);
+
+    List<BlogPost> findAllBlogPostByUserAndDraft(User user, boolean draft);
 }
