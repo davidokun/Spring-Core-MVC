264b9c4bacc7485e8d41d7ce44530fce6bd29c69
davidokun <davidokun99@gmail.com>
2017-10-03 20:45:37 -0500

    xmlConfig - Add base project

---
+++ spring-mvc-java-config-web-xml/docker/mysql/script.sql
@@ -0,0 +1,15 @@
+CREATE SCHEMA test;
+
+CREATE TABLE test.user
+(
+    id INT,
+    username VARCHAR(50),
+    password VARCHAR(50),
+    enable TINYINT
+);
+
+INSERT INTO test.user
+(id, username, password, enable)
+    VALUES
+    (1, 'testuser','testpassword',true),
+    (2, 'testuser2','testpassword2',true)
---
+++ spring-mvc-java-config-web-xml/src/main/java/com/singletonapps/config/WebConfig.java
@@ -0,0 +1,87 @@
+package com.singletonapps.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.*;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import java.util.Locale;
+
+/**
+ * Configuration class dispatcher-servlet.xml
+ */
+@EnableWebMvc
+@Configuration
+@Import({DataBaseContext.class})
+@ComponentScan(basePackages = "com.singletonapps")
+@PropertySource(value = "classpath:application.properties")
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/newblogpost.html").setViewName("/newblogpost");
+        registry.addViewController("/accessDenied").setViewName("accessDenied");
+        registry.addViewController("/admin.html").setViewName("/admin/admin");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasename("lang");
+        messageSource.setDefaultEncoding("UTF-8");
+
+        return messageSource;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("language");
+
+        return localeChangeInterceptor;
+    }
+
+    @Bean
+    public CookieLocaleResolver localeResolver() {
+
+        final CookieLocaleResolver cookieLocaleResolver = new CookieLocaleResolver();
+        cookieLocaleResolver.setDefaultLocale(Locale.ENGLISH);
+
+        return cookieLocaleResolver;
+    }
+
+    @Bean
+    public InternalResourceViewResolver internalResourceViewResolver() {
+
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/pages/");
+        resolver.setSuffix(".jsp");
+
+        return resolver;
+    }
+}
