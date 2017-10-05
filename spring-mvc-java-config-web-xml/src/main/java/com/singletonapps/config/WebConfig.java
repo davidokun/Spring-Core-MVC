1529a5f3cdec666f87e26ea9181b708f821f8346
davidokun <davidokun99@gmail.com>
2017-10-05 17:22:37 -0500

    xmlConfig - Add EnableWebMvc annotation.

--- spring-mvc-java-config-web-xml/src/main/java/com/singletonapps/config/WebConfig.java
+++ spring-mvc-java-config-web-xml/src/main/java/com/singletonapps/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.singletonapps.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -10,6 +11,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 /**
  * Configuration class dispatcher-servlet.xml
  */
+@EnableWebMvc
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
 
