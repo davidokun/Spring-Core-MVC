b31e3437492c643e540e1c2bc02aba807c90f905
davidokun <davidokun99@gmail.com>
2017-10-12 17:35:37 -0500

    xmlConfig - Import SecurityConfig in WebConfig.java

--- spring-mvc-java-config-web-xml/src/main/java/com/singletonapps/config/WebConfig.java
+++ spring-mvc-java-config-web-xml/src/main/java/com/singletonapps/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.singletonapps.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -13,6 +14,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
  */
 @EnableWebMvc
 @Configuration
+@Import(value = SecurityConfig.class)
 public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
